16418bc27a0da7ce0173de04b2e26402ded720cf
nex2null <nex2null@gmail.com>
2021-04-01 19:29:36 -0400

    Showing equipment information

--- src/VueComponents/InventoryComponent.vue
+++ src/VueComponents/InventoryComponent.vue
@@ -16,11 +16,40 @@
     <br />
     Quantity:
     {{ selectedItem ? selectedItem.amount : "" }}
+    <div v-if="isEquipment">
+      <div><strong>Rarity:</strong> {{ selectedItem.rarity }}</div>
+      <div>
+        <strong>Implicits:</strong>
+        <fieldset style="width: 300px; margin: auto;">
+          <div
+            v-for="(implicit, index) in selectedItem.implicits"
+            :key="index"
+            :value="index"
+          >
+            {{ implicit.stat }} : {{ implicit.value }}
+          </div>
+        </fieldset>
+      </div>
+      <div>
+        <strong>Affixes:</strong>
+        <fieldset style="width: 300px; margin: auto;">
+          <div
+            v-for="(affix, index) in selectedItem.affixes"
+            :key="index"
+            :value="index"
+          >
+            {{ affix.type }} : {{ affix.value }}
+          </div>
+        </fieldset>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Item from "@/Game/Itemization/Item";
+import { itemInformations } from '@/Game/Itemization/ItemInformation';
+import ItemSuperTypeEnum from '@/Game/Itemization/Enums/ItemSuperTypeEnum';
 import { Component, Vue } from "vue-property-decorator";
 import Game from "../Game/Game";
 
@@ -28,11 +57,13 @@ import Game from "../Game/Game";
 export default class InventoryComponent extends Vue {
   game: Game;
   selectedItem: Item | null;
+  isEquipment: boolean;
 
   constructor() {
     super();
     this.game = Game.getInstance();
     this.selectedItem = null;
+    this.isEquipment = false;
   }
 
   isItemSelected(item: Item | null) {
@@ -48,7 +79,13 @@ export default class InventoryComponent extends Vue {
       return;
     }
 
-    this.selectedItem = this.game.town.inventory.items[event.target.value];
+    var selectedItem = this.game.town.inventory.items[event.target.value];
+    if (!selectedItem)
+      return;
+    
+    this.selectedItem = selectedItem;
+    var itemInformation = itemInformations.find(x => x.itemType == selectedItem.type);
+    this.isEquipment = itemInformation != null && itemInformation.itemSuperType == ItemSuperTypeEnum.Equipment;
   }
 }
 </script>
