8cd869289da96da1b3dc8e3a24d22a9c8ed66df7
nex2null <nex2null@gmail.com>
2021-04-27 08:21:46 -0400

    Getting a blessed UI drawn

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,45 @@
 #!/usr/bin/env node
 
 import chalk from 'chalk';
+import blessed from 'blessed';
 import Game from './Game/Game';
 
 console.log(`Total Gold: ${chalk.yellow(Game.getInstance().town.totalGold)}`);
 
+// Initialize the screen widget.
+var screen = blessed.screen({
+  smartCSR: true,
+  title: 'Hello World'
+});
+
+screen.key('q', () => {
+  screen.destroy();
+});
+
+// Create a box perfectly centered horizontally and vertically.
+var box = blessed.box({
+  top: 'center',
+  left: 'center',
+  width: '50%',
+  height: '50%',
+  content: 'Hello {bold}world{/bold}!',
+  tags: true,
+  border: {
+    type: 'line'
+  },
+  style: {
+    fg: 'white',
+    bg: 'magenta',
+    border: {
+      fg: '#f0f0f0'
+    },
+    hover: {
+      bg: 'green'
+    }
+  }
+});
+
+// Append our box to the screen.
+screen.append(box);
+
+screen.render();
