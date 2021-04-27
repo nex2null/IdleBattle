080c6d389d7ed2490ee786831695ce29abe78c7c
nex2null <nex2null@gmail.com>
2021-04-27 08:54:16 -0400

    Cleaning up some code

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,12 @@
 #!/usr/bin/env node
 
-import chalk from 'chalk';
-import blessed from 'blessed';
+const blessed = require('blessed');
 import Game from './Game/Game';
 
-console.log(`Total Gold: ${chalk.yellow(Game.getInstance().town.totalGold)}`);
-
-// Initialize the screen widget.
+// Create a screen object.
 var screen = blessed.screen({
   smartCSR: true,
-  title: 'Hello World'
-});
-
-screen.key('q', () => {
-  screen.destroy();
+  title: 'Command Line Idle - RPG'
 });
 
 // Create a box perfectly centered horizontally and vertically.
@@ -22,14 +15,17 @@ var box = blessed.box({
   left: 'center',
   width: '50%',
   height: '50%',
-  content: 'Hello {bold}world{/bold}!',
+  content: `
+  {bold}UNDER CONSTRUCTION!{/bold} - Great things will be here soon.\n\n
+  You currently have {yellow-fg}${Game.getInstance().town.totalGold} gold{/yellow-fg}.\n\n
+  Please press {bold}ESCAPE{/bold} to exit.
+  `,
   tags: true,
   border: {
     type: 'line'
   },
   style: {
     fg: 'white',
-    bg: 'magenta',
     border: {
       fg: '#f0f0f0'
     },
@@ -42,4 +38,10 @@ var box = blessed.box({
 // Append our box to the screen.
 screen.append(box);
 
+// Quit on Escape, q, or Control-C.
+screen.key(['escape', 'C-c'], () => {
+  return process.exit(0);
+});
+
+// Render the screen.
 screen.render();
