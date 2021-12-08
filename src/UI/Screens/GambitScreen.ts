0a0cbe93826e36703396d7a0cb0b39f24f92a679
nex2null <nex2null@gmail.com>
2021-12-08 10:20:05 -0500

    Stopping point

--- src/UI/Screens/GambitScreen.ts
+++ src/UI/Screens/GambitScreen.ts
@@ -8,19 +8,32 @@ import IScreen from "./IScreen";
 import TownScreen from "./TownScreen";
 import Town from "../../Game/Town";
 import PlayerCharacter from "../../Game/PlayerCharacter";
+import GambitConditionEnum from "../../Game/BattleSystem/Enums/GambitConditionEnum";
+import Gambit from "../../Game/BattleSystem/Gambits/Gambit";
+import SkillEnum from "../../Game/BattleSystem/Enums/SkillEnum";
 
 class GambitScreen implements IScreen {
 
   // Properties
   screen: any;
   screenElements: any = {};
-  currentPlayerIndex: number = 0;
+  currentCharacter?: PlayerCharacter;
+  currentGambit?: Gambit;
   town: Town;
+  gambitConditions: Array<string> = new Array<string>();
+
+  // const color: Color = Color[colorString as keyof typeof Color];
+  // BLAH this.gambitConditions.push(enumMember as keyof typeof GambitConditionEnum);
 
   // Constructor
   constructor() {
     this.screen = null;
     this.town = Game.getInstance().town;
+    
+    // Populate gambits
+    for (var enumMember in GambitConditionEnum) {
+      this.gambitConditions.push(GambitConditionEnum[enumMember as keyof typeof GambitConditionEnum]);
+    }
   }
 
   //
@@ -52,12 +65,37 @@ class GambitScreen implements IScreen {
       keys: true
     });
 
-    // Gambit display
-    this.screenElements.gambitBox = blessed.box({
+    // Gambit table
+    this.screenElements.gambitTable = contrib.table({
       top: 0,
       left: 21,
-      width: 80,
+      width: 68,
       height: 50,
+      keys: true,
+      fg: 'white',
+      selectedFg: 'white',
+      selectedBg: 'blue',
+      interactive: true,
+      hidden: true,
+      columnSpacing: 5, // in chars
+      columnWidth: [20, 20, 20], // in chars
+      scrollbar: {
+        style: {
+          bg: 'white'
+        }
+      },
+      border: {
+        type: 'line'
+      }
+    });
+
+    // Edit gambit box
+    this.screenElements.editGambitBox = blessed.box({
+      top: 0,
+      left: 21,
+      width: 68,
+      height: 50,
+      hidden: true,
       border: {
         type: 'line'
       },
@@ -69,34 +107,44 @@ class GambitScreen implements IScreen {
       }
     });
 
-    // Gambit table
-    this.screenElements.gambitTable = contrib.table({
-      parent: this.screenElements.gambitBox,
-      keys: true,
-      fg: 'white',
-      selectedFg: 'white',
-      selectedBg: 'blue',
-      interactive: true,
-      width: 68,
-      height: 38,
-      hidden: true,
-      columnSpacing: 5, // in chars
-      columnWidth: [20, 20, 20], // in chars
-      scrollbar: {
-        style: {
-          bg: 'white'
+    // Gambit condition list
+    this.screenElements.gambitConditionList = blessed.list({
+      parent: this.screenElements.editGambitBox,
+      top: 0,
+      left: 0,
+      width: 20,
+      height: 40,
+      border: {
+        type: 'line'
+      },
+      label: 'Condition',
+      style: {
+        selected: {
+          bg: 'white',
+          fg: 'black'
+        },
+        border: {
+          fg: 'white'
+        },
+        focus: {
+          border: {
+            fg: 'blue'
+          }
         }
-      }
+      },
+      keys: true
     });
 
     // Set key bindings
     this.screenElements.menu.key(['escape'], () => this.screenElements.menu.select(this.screenElements.menu.fuzzyFind('Exit')));
     this.screenElements.menu.on('select', (el: any) => this.onMenuSelect(el));
     this.screenElements.gambitTable.rows.key(['escape'], () => this.unloadCharacterGambits());
+    this.screenElements.gambitTable.rows.key(['e'], () => this.editSelectedGambit());
 
     // Append items to screen
     this.screen.append(this.screenElements.menu);
-    this.screen.append(this.screenElements.gambitBox);
+    this.screen.append(this.screenElements.gambitTable);
+    this.screen.append(this.screenElements.editGambitBox);
 
     // Load the menu
     this.loadMenu();
@@ -142,8 +190,11 @@ class GambitScreen implements IScreen {
     if (!playerCharacter)
       return;
 
+    // Keep track of current character
+    this.currentCharacter = playerCharacter;
+
     // Load character gambits
-    this.loadCharacterGambits(playerCharacter);
+    this.loadCharacterGambits(this.currentCharacter);
     this.screenElements.gambitTable.show();
     this.screenElements.gambitTable.focus();
 
@@ -177,6 +228,43 @@ class GambitScreen implements IScreen {
     this.screenElements.gambitTable.hide();
     this.screenElements.menu.focus();
     this.screen.render();
+  }
+
+  //
+  // Edit the selected gambit
+  //
+  private editSelectedGambit() {
+
+    // Sanity check current character
+    if (!this.currentCharacter) return;
+
+    // Grab the gambit being edited
+    var gambitIndex = this.screenElements.gambitTable.rows.selected;
+    var gambit = this.currentCharacter.gambits[gambitIndex];
+    this.currentGambit = gambit;
+
+    // Render gambit edit details
+    this.renderGambitEditDetails(gambit);
+
+    // Hide the gambit table and show the edit form
+    this.screenElements.gambitTable.hide();
+    this.screenElements.editGambitBox.show();
+
+    // Focus on the condition list
+    this.screenElements.gambitConditionList.focus();
+    this.screen.render();
+  }
+
+  //
+  // Render the edit gambit details box
+  //
+  private renderGambitEditDetails(gambit?: Gambit) {
+
+    // If we don't have an existing gambit, just start with a default one
+    gambit = gambit || new Gambit(GambitConditionEnum.EnemyAny, null, SkillEnum.Attack);
+
+    // Set the condition items
+    this.screenElements.gambitConditionList.setItems(this.gambitConditions);
   }
 }
 
