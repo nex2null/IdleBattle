0b789649e0228898c6a2cb25e8711249e36e74bc
nex2null <nex2null@gmail.com>
2021-12-08 08:06:46 -0500

    Further implementing gambit screen

--- src/UI/Screens/GambitScreen.ts
+++ src/UI/Screens/GambitScreen.ts
@@ -1,20 +1,26 @@
 // Imports
 const blessed = require('blessed');
+const contrib = require('blessed-contrib');
 import Game from "../../Game/Game";
 import ScreenManager from "../ScreenManager";
 import GameSaver from "../../IO/GameSaver";
 import IScreen from "./IScreen";
 import TownScreen from "./TownScreen";
 import Town from "../../Game/Town";
+import PlayerCharacter from "../../Game/PlayerCharacter";
 
 class GambitScreen implements IScreen {
 
   // Properties
   screen: any;
+  screenElements: any = {};
+  currentPlayerIndex: number = 0;
+  town: Town;
 
   // Constructor
   constructor() {
     this.screen = null;
+    this.town = Game.getInstance().town;
   }
 
   //
@@ -24,12 +30,12 @@ class GambitScreen implements IScreen {
 
     this.screen = screen;
 
-    var gambitMenu = blessed.list({
-      top: 8,
-      left: '40',
-      width: '25%',
-      height: '25%+1',
-      tags: true,
+    // Character menu
+    this.screenElements.menu = blessed.list({
+      top: 0,
+      left: 0,
+      width: 20,
+      height: 50,
       border: {
         type: 'line'
       },
@@ -46,18 +52,57 @@ class GambitScreen implements IScreen {
       keys: true
     });
 
-    // Set menu items
-    var items = Game.getInstance().town.playerCharacters.map(x => x.name);
-    items.push('Exit');
-    gambitMenu.setItems(items);
+    // Gambit display
+    this.screenElements.gambitBox = blessed.box({
+      top: 0,
+      left: 21,
+      width: 80,
+      height: 50,
+      border: {
+        type: 'line'
+      },
+      style: {
+        fg: 'white',
+        border: {
+          fg: 'white'
+        }
+      }
+    });
 
-    // Handle escape keybind on menu
-    gambitMenu.key(['escape'], () => gambitMenu.select(gambitMenu.fuzzyFind('Exit')));
-    gambitMenu.on('select', (el: any) => this.onMenuSelect(el));
+    // Gambit table
+    this.screenElements.gambitTable = contrib.table({
+      parent: this.screenElements.gambitBox,
+      keys: true,
+      fg: 'white',
+      selectedFg: 'white',
+      selectedBg: 'blue',
+      interactive: true,
+      width: 68,
+      height: 38,
+      hidden: true,
+      columnSpacing: 5, // in chars
+      columnWidth: [20, 20, 20], // in chars
+      scrollbar: {
+        style: {
+          bg: 'white'
+        }
+      }
+    });
 
-    // Add menu to screen
-    this.screen.append(gambitMenu);
-    gambitMenu.focus();
+    // Set key bindings
+    this.screenElements.menu.key(['escape'], () => this.screenElements.menu.select(this.screenElements.menu.fuzzyFind('Exit')));
+    this.screenElements.menu.on('select', (el: any) => this.onMenuSelect(el));
+    this.screenElements.gambitTable.rows.key(['escape'], () => this.unloadCharacterGambits());
+
+    // Append items to screen
+    this.screen.append(this.screenElements.menu);
+    this.screen.append(this.screenElements.gambitBox);
+
+    // Load the menu
+    this.loadMenu();
+
+    // Focus
+    this.screenElements.menu.focus();
   }
 
   //
@@ -72,6 +117,12 @@ class GambitScreen implements IScreen {
   // Uninitializes the screen
   //
   public uninitializeScreen() {
+  }
+
+  private loadMenu() {
+    var items = this.town.playerCharacters.map(x => x.name);
+    items.push('Exit');
+    this.screenElements.menu.setItems(items);
   }
 
   //
@@ -91,7 +142,41 @@ class GambitScreen implements IScreen {
     if (!playerCharacter)
       return;
 
-    // TODO: Load gambits
+    // Load character gambits
+    this.loadCharacterGambits(playerCharacter);
+    this.screenElements.gambitTable.show();
+    this.screenElements.gambitTable.focus();
+
+    // Render the screen
+    this.screen.render();
+  }
+
+  //
+  // Display the selected character gambits
+  //
+  private loadCharacterGambits(character: PlayerCharacter) {
+
+    // Clear any existing rows in the table
+    var tableItems = this.screenElements.gambitTable.rows;
+    if (tableItems) {
+      tableItems.clearItems();
+      tableItems._listInitialized = false;
+    }
+
+    // Set the tables data
+    this.screenElements.gambitTable.setData({
+      headers: [' Condition', ' Input', ' Skill'],
+      data: character.gambits.map(x => [x.conditionEnum, x.conditionInput || 'N/A', x.skillEnum])
+    });
+  }
+
+  //
+  // Unloads character gambits
+  //
+  private unloadCharacterGambits() {
+    this.screenElements.gambitTable.hide();
+    this.screenElements.menu.focus();
+    this.screen.render();
   }
 }
 
